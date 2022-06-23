f02568762d80798f65549f3bd99c724f280c1cfa
BarritoN78 <95886922+BarritoN78@users.noreply.github.com>
2022-06-23 10:58:26 -0400

    Cleaned up the code and added functionality
    Inserted/changed comments and converted the previous account registration to be specifically for users(added a prompt for their name)

--- Project_0_Banking_App/src/com/revature/p0/main/LoginScreen.java
+++ Project_0_Banking_App/src/com/revature/p0/main/LoginScreen.java
@@ -2,18 +2,18 @@ package com.revature.p0.main;
 import java.util.Scanner;
 public class LoginScreen {
 
-	//Declare Variables
-	private String email, password, entry;
+	/*Class Variables*/
+	private String email, password, command;
 	private Boolean appAccess = false;
 	private static Scanner key_inp = new Scanner(System.in);
 	
-	//Constructor
+	/*Constructor*/
 	public LoginScreen() {
 		while (appAccess == false)
 		{
 			System.out.println("Enter 'L' to login or 'R' to register\n");
-			entry = key_inp.nextLine();
-			switch (entry.toUpperCase()) {
+			command = key_inp.nextLine();
+			switch (command.toUpperCase()) {
 			case "L": 
 				login(); break;
 			case "R": 
@@ -26,10 +26,13 @@ public class LoginScreen {
 		key_inp.close();
 	}
 	
-	//Logging in to the app
+	/*Logging in to the app*/
 	private void login() {
+		/*Local Variables*/
 		Boolean loginSuccess = false, toHome = false;
-		do {
+		
+		/*Function*/
+		do {//Login Email Loop
 			System.out.println("Please enter your email or 'H' to go back\n");
 			email = key_inp.nextLine();
 			switch (email.toUpperCase()) {
@@ -48,10 +51,13 @@ public class LoginScreen {
 		}while(loginSuccess == false && toHome == false);
 	}
 	
-	//Prompt for password and test validity
+	/*Prompt for password and test validity*/
 	private void loginPasswordEntry() {
+		/*Local Variables*/
 		Boolean toHome = false;
-		do {
+		
+		/*Function*/
+		do {//Login Password Loop
 			System.out.println("Please enter your password or 'H' to return to the home screen\n");
 			password = key_inp.nextLine();
 			switch(password.toUpperCase()) {
@@ -71,26 +77,48 @@ public class LoginScreen {
 		}while (toHome == false);
 	}
 	
-	//Registering a new account for the app
+	/*Registering a new user for the app*/
 	private void register() {
+		/*Local Variables*/
 		int emailLength;
-		Boolean toHome = false;
-		do {
+		Boolean toHome = false, nameConfirm = false;
+		String fName = "", mInit = "", lName = "";
+		
+		/*Function*/
+		do {//Name Loop
+			System.out.println("Please enter your first name\n");
+			fName = key_inp.nextLine();
+			System.out.println("Please enter your middle initial\n");
+			mInit = key_inp.nextLine();
+			System.out.println("Please enter your last name\n");
+			lName = key_inp.nextLine();
+			System.out.println("Your is name is" + fName + " " + mInit + ". " + lName + "\n");
+			System.out.println("Enter 'Y' to confirm. All other commands will reset the name entry\n");
+			command = key_inp.nextLine();
+			switch(command.toUpperCase()) {
+			case "Y":
+				nameConfirm = true;break;
+			default:
+				nameConfirm = false;break;
+			}
+		}while(nameConfirm == false);		
+		
+		do {//Email Loop
 			System.out.println("Please enter your email or 'H' to go back\n");
 			email = key_inp.nextLine();
 			emailLength = email.length();
-			//Testing email validity
-			try {
+			
+			try {//Testing email validity
 				if (email.contains("@") && email.substring(emailLength - 5).contains(".")) {//email has '@' and '.' towards the end
 					switch(email) {
 					case "Already@exists.":
-						System.out.println("An account has already been linked to this email. Please use a different email or log in\n");break;
-					default:
+						System.out.println("A user account has already been linked to this email. Please use a different email or log in\n");break;
+					default://Password
 						registerPasswordEntry();
 						toHome = true; break;
 					}
 				}
-				else {
+				else {//Invalid email
 					switch(email.toUpperCase()) {
 					case "H":
 						toHome = true; break;
@@ -106,33 +134,39 @@ public class LoginScreen {
 		}while (toHome == false);
 	}
 	
-	//Prompt for a new password and then confirm it
+	/*Prompt for a new password and then confirm it*/
 	public void registerPasswordEntry() {
+		/*Local Variables*/
 		String password1, password2;
 		Boolean toHome = false;
+		
+		/*Function*/
 		do {
-		System.out.println("Please enter a password for the new account or 'C' to cancel the account creation\n");
-		password1 = key_inp.nextLine();
-		if (password1.length() < 8) {
-			switch(password1.toUpperCase()) {
-			case "C":
-				System.out.println("Account creation canceled, you will be returned to the home screen");
-				toHome = true; break;
-			default:
-				System.out.println("The password must be at least 8 characters long\n");break;
+			System.out.println("Please enter a password for the new user or 'C' to cancel the account creation\n");
+			password1 = key_inp.nextLine();
+			
+			if (password1.length() < 8) {//Invalid password entry
+				switch(password1.toUpperCase()) {
+				case "C":
+					System.out.println("User creation canceled, you will be returned to the home screen");
+					toHome = true; break;
+				default:
+					System.out.println("The password must be at least 8 characters long\n");break;
+				}
 			}
-		}
-		else { //Reenter password to confirm
-			System.out.println("Please reenter the password to confirm\n");
-			password2 = key_inp.nextLine();			
-			if (password2.equals(password1)) {
-				System.out.println("Your account has been created. You can now log in\n");
-				toHome = true;
+			else { //Reenter password to confirm
+				System.out.println("Please reenter the password to confirm\n");
+				password2 = key_inp.nextLine();	
+				
+				if (password2.equals(password1)) {//Matched
+					System.out.println("Your user account has been created. You can now log in\n");
+					toHome = true;
+					//INSERT email, password, TO users
+				}
+				else {//Didn't match
+					System.out.println("The passwords you entered did not match. Please try again\n");
+				}
 			}
-			else {
-				System.out.println("The passwords you entered did not match. Please try again\n");
-			}
-		}
 		}while(toHome == false);
 	}
 }
